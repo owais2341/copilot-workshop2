358b86515e817addc18dd200dfe725bbda73fa5c
smowais.ali <gameranger.9213@gmail.com>
2026-01-15 23:39:55 +0500

    Express app with Nodemon setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the Express app!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
